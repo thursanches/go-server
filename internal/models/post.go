a6ac2e6ef8c5c455a34f2d81d14be6a3b5dfad80
Arthur <arthursks7@gmail.com>
2026-03-17 22:37:21 -0300

    created field published

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,6 +1,7 @@
 package models
 
 type Post struct {
-	ID   int    `json:"id"`
-	Body string `json:"body"`
+	ID        int    `json:"id"`
+	Body      string `json:"body"`
+	Published bool   `json:"published"`
 }
