8293668bb79b603385b70fc0f0558661374c6398
Arthur <arthursks7@gmail.com>
2026-03-17 22:36:57 -0300

    add field published

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -10,31 +10,53 @@ type PostRepository struct {
 }
 
 func NewPostRepository(db *sql.DB) *PostRepository {
-	// Criamos a tabela automaticamente no início
 	query := `CREATE TABLE IF NOT EXISTS posts (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		body TEXT
+		body TEXT,
+		published BOOLEAN DEFAULT 0
 	);`
 	db.Exec(query)
-
 	return &PostRepository{db: db}
 }
 
 func (r *PostRepository) Create(p models.Post) models.Post {
-	res, _ := r.db.Exec("INSERT INTO posts (body) VALUES (?)", p.Body)
+	res, _ := r.db.Exec("INSERT INTO posts (body, published) VALUES (?, ?)", p.Body, p.Published)
 	id, _ := res.LastInsertId()
 	p.ID = int(id)
 	return p
 }
 
 func (r *PostRepository) GetAll() []models.Post {
-	rows, _ := r.db.Query("SELECT id, body FROM posts")
+	rows, err := r.db.Query("SELECT id, body, published FROM posts")
+	if err != nil {
+		return []models.Post{}
+	}
 	defer rows.Close()
 
-	var ps []models.Post
+	var ps []models.Post = []models.Post{}
 	for rows.Next() {
 		var p models.Post
-		rows.Scan(&p.ID, &p.Body)
+		var published int
+		rows.Scan(&p.ID, &p.Body, &published)
+		p.Published = published == 1
+		ps = append(ps, p)
+	}
+	return ps
+}
+
+func (r *PostRepository) GetAllPublished() []models.Post {
+	rows, err := r.db.Query("SELECT id, body, published FROM posts WHERE published = 1")
+	if err != nil {
+		return []models.Post{}
+	}
+	defer rows.Close()
+
+	var ps []models.Post = []models.Post{}
+	for rows.Next() {
+		var p models.Post
+		var published int
+		rows.Scan(&p.ID, &p.Body, &published)
+		p.Published = published == 1
 		ps = append(ps, p)
 	}
 	return ps
@@ -42,19 +64,26 @@ func (r *PostRepository) GetAll() []models.Post {
 
 func (r *PostRepository) GetByID(id int) (models.Post, bool) {
 	var p models.Post
-	err := r.db.QueryRow("SELECT id, body FROM posts WHERE id = ?", id).Scan(&p.ID, &p.Body)
+	var published int
+	err := r.db.QueryRow("SELECT id, body, published FROM posts WHERE id = ?", id).
+		Scan(&p.ID, &p.Body, &published)
 	if err == sql.ErrNoRows {
 		return p, false
 	}
+	p.Published = published == 1
 	return p, true
 }
 
-func (r *PostRepository) Update(id int, body string) (models.Post, bool) {
-	_, err := r.db.Exec("UPDATE posts SET body = ? WHERE id = ?", body, id)
+func (r *PostRepository) Update(id int, body string, published bool) (models.Post, bool) {
+	res, err := r.db.Exec("UPDATE posts SET body = ?, published = ? WHERE id = ?", body, published, id)
 	if err != nil {
 		return models.Post{}, false
 	}
-	return models.Post{ID: id, Body: body}, true
+	count, _ := res.RowsAffected()
+	if count == 0 {
+		return models.Post{}, false
+	}
+	return models.Post{ID: id, Body: body, Published: published}, true
 }
 
 func (r *PostRepository) Delete(id int) bool {
